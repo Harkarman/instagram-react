0fafc114b4acc1d423fb5b48a6714e3c0209a827
Harkarman Singh <harkarman.web@gmail.com>
2021-05-22 01:08:29 +0530

    create basic UI for suggestions

--- src/components/sidebar/Suggestions.js
+++ src/components/sidebar/Suggestions.js
@@ -1,6 +1,20 @@
 import { useState, useEffect } from "react";
-
+import PropTypes from "prop-types";
+import Skeleton from "react-loading-skeleton";
 export default function Suggestions({ userId }) {
-  const [profiles, setProfiles] = null;
-  return <p>Suggestion</p>;
+  const [profiles, setProfiles] = useState(null);
+  return !profiles ? (
+    <Skeleton count={1} height={150} className="mt-5" />
+  ) : profiles.length > 0 ? (
+    <div>
+      <div className="rounded flex flex-col"></div>
+      <div className="text-sm flex items-center align-items justify-between mb-2">
+        <p className="font-bold text-grey-base">Suggestion</p>;
+      </div>
+    </div>
+  ) : null;
 }
+
+Suggestions.propTypes = {
+  userId: PropTypes.string,
+};
