0a2da91d10c07ff3c99ac2100671cf0e598b53ca
Harkarman Singh <harkarman.web@gmail.com>
2021-06-28 23:35:08 +0530

    finish profile header

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -74,7 +74,34 @@ export default function Header({
             )
           )}
         </div>
-        <div className="container flex mt-4"></div>
+        <div className="container flex mt-4">
+          {followers === undefined || following === undefined ? (
+            <Skeleton count={1} width={677} height={24} />
+          ) : (
+            <>
+              <p className="mr-10">
+                <span className="font-bold">{photosCount}</span>
+                {` `}
+                {photosCount === 1 ? `photo` : `photos`}
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{followers.length}</span>
+                {` `}
+                {followers.length === 1 ? `follower` : `followers`}
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{following.length}</span>
+                {` `}
+                following
+              </p>
+            </>
+          )}
+        </div>
+        <div className="container mt-4">
+          <p className="font-medium">
+            {!fullname ? <Skeleton count={1} height={24} /> : fullname}
+          </p>
+        </div>
       </div>
     </div>
   );
